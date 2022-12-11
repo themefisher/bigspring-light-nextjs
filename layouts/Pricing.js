b0cace4b83b4cf6004c82e793047cb651d10616e
Tfferdous <ferdous.themefisher@gmail.com>
2022-12-11 15:53:34 +0600

    added missing dolar sign to pricing card

--- layouts/Pricing.js
+++ layouts/Pricing.js
@@ -21,7 +21,7 @@ function Pricing({ data }) {
                 <div className="card text-center">
                   <h4>{plan.title}</h4>
                   <div className="mt-5">
-                    <span className="text-5xl text-text">{plan.price}</span>
+                    <span className="text-5xl text-text">${plan.price}</span>
                     <span>/ {plan.type}</span>
                   </div>
                   <h5 className="mt-2 font-normal text-dark">
