1526e26737b928de45779bc7c0efea36ff6f960c
Harper Foley <harper.e.foley@gmail.com>
2023-10-24 16:54:49 -0700

    Updates the webhook handler to use the new `req.text()` method instead of the deprecated `req.blob()` method.

--- pages/api/v1/webhooks/emails.js
+++ pages/api/v1/webhooks/emails.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
     try {
       event = stripe.webhooks.constructEvent(
-        await (await req.blob()).text(),
+        await (await req.text()),
         req.headers["stripe-signature"],
         webhookSecret
       );
