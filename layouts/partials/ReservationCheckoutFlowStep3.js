bba632e4e857aa2b77edfc09fe7b02ef7c3ce138
Harper Foley <harper.e.foley@gmail.com>
2023-10-31 15:07:53 -0700

    Updates copy to include Steph's story (#18) Removes styling from text to make it look like a letter

--- layouts/partials/ReservationCheckoutFlowStep3.js
+++ layouts/partials/ReservationCheckoutFlowStep3.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 function ReservationCheckoutFlowStep3({ onClose, formData }) {
   return (
-    <section className="fixed inset-0 z-50 flex items-center justify-center p-4 overflow-y-auto">
+    <section className="fixed inset-0 z-50 flex items-center justify-center p-4 mt-4 overflow-y-auto">
       <div className='flex flex-col md:flex-row'>
         <div className='flex-grow bg-[#1C3F60] rounded-md py-10 px-10'>
           <Image
