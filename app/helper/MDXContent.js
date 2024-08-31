5d7bf2d52734da37dfbbea4a5abaa662d1cfedb7
tfrubel <rubel.themefisher@gmail.com>
2024-08-31 11:34:34 +0600

    update syntex highlighting theme

--- app/helper/MDXContent.js
+++ app/helper/MDXContent.js
@@ -1,5 +1,5 @@
 import shortcodes from "@layouts/shortcodes/all";
-import "highlight.js/styles/paraiso-light.min.css";
+import "highlight.js/styles/atom-one-dark.css";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
